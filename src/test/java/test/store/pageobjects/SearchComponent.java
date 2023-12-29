c449dc6d1123d8f77ecfa4dcf392b6b0af650071
andimon <andrevella2001@gmail.com>
2023-12-29 22:58:41 +0100

    feat(page-object): search component, allowing user to search item in store via a text field and a search button

---
+++ src/test/java/test/store/pageobjects/SearchComponent.java
@@ -0,0 +1,25 @@
+package test.store.pageobjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+//The search component consists of a text field and a search button.
+public class SearchComponent {
+    private WebDriver webDriver;
+    public SearchComponent(WebDriver webDriver) {
+        this.webDriver = webDriver;
+    }
+
+    public void inputExpression(String expression){
+        webDriver.findElement(By.name("header-search-text")).sendKeys(expression);
+    }
+
+    public void clickSearchButton(){
+        webDriver.findElement(By.xpath("//i[contains(@class, 'fas fa-search')]")).click();
+    }
+
+    public void search(String expression){
+        inputExpression(expression);
+        clickSearchButton();
+    }
+}
