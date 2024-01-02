5794d9ea3f591b8366a86feea30d52ade10f5c31
andimon <andrevella2001@gmail.com>
2024-01-02 13:13:34 +0100

    chore: add operator for model based testing

--- src/main/java/edu/um/cps3230/WebStoreOperator.java
+++ src/main/java/edu/um/cps3230/WebStoreOperator.java
@@ -1,4 +1,46 @@
 package edu.um.cps3230;
 
+import edu.um.cps3230.pageobjects.ProductDetailsComponent;
+import edu.um.cps3230.pageobjects.ProductsViewComponent;
+import edu.um.cps3230.pageobjects.NavigationComponent;
+import org.openqa.selenium.WebDriver;
+
 public class WebStoreOperator {
+    private WebDriver webDriver;
+    private NavigationComponent searchComponent;
+    private ProductsViewComponent productsViewComponent;
+
+    private ProductDetailsComponent productDetailsComponent;
+
+    public WebStoreOperator(WebDriver webDriver) {
+        this.webDriver = webDriver;
+        searchComponent = new NavigationComponent(webDriver);
+        productsViewComponent = new ProductsViewComponent(webDriver);
+        productDetailsComponent = new ProductDetailsComponent(webDriver);
+    }
+
+    public void initiateWebStore() {
+        webDriver.manage().window().maximize();
+        webDriver.get("https://www.klikk.com.mt");
+    }
+
+    public void searchProduct(String searchQuery) {
+        searchComponent.search(searchQuery);
+    }
+
+    public void returnToHome() {
+        searchComponent.returnToHomePage();
+    }
+
+    public boolean isProductPageEmpty(){
+        return productsViewComponent.getNumberOfProductsInPage()==0;
+    }
+
+    public void selectFirstProduct(){
+        productsViewComponent.selectFirstProduct();
+    }
+
+    public boolean isProductInStock(){
+        return productDetailsComponent.getStockStatus().equals("In stock");
+    }
 }
