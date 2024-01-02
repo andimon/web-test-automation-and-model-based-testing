46703f6780ec2442b9aa26d63311efdde3a6d6d9
andimon <andrevella2001@gmail.com>
2024-01-02 13:14:05 +0100

    chore: search component -> navigation component

--- src/main/java/edu/um/cps3230/pageobjects/NavigationComponent.java
+++ src/main/java/edu/um/cps3230/pageobjects/NavigationComponent.java
@@ -4,11 +4,16 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 //The search component consists of a text field and a search button.
-public class SearchComponent {
+public class NavigationComponent {
     private WebDriver webDriver;
-    public SearchComponent(WebDriver webDriver) {
+    public NavigationComponent(WebDriver webDriver) {
         this.webDriver = webDriver;
     }
+
+    public void returnToHomePage(){
+        webDriver.findElement(By.id("home_logo")).click();
+    }
+
 
     public void inputExpression(String expression){
         webDriver.findElement(By.name("header-search-text")).sendKeys(expression);
